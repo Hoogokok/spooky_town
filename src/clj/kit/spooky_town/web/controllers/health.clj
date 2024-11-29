ad2ce62607886c95a26a83e07ec5d8088bb85656
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-11-30 00:29:01 +0900

    refactor(db): 트랜잭션 매니저를 사용하게 수정

--- src/clj/kit/spooky_town/web/controllers/health.clj
+++ src/clj/kit/spooky_town/web/controllers/health.clj
@@ -1,26 +1,31 @@
 (ns kit.spooky-town.web.controllers.health
   (:require
-    [kit.spooky-town.web.responses :as responses]
-    [kit.edge.db.sql.conman :as db])
+   [kit.spooky-town.web.responses :as responses]
+   [kit.spooky-town.db.transaction :as tx])
   (:import
-    [java.util Date]))
+   [java.util Date]))
 
 (defn healthcheck!
-  [{:keys [db] :as req}]
+  [{:keys [tx-manager] :as req}]
   (try
-    (let [db-status (db/health-check db)]
+    (let [db-status (try
+                      (tx/with-read-only
+                        tx-manager
+                        (fn [_] (= 1 1)))
+                      true
+                      (catch Exception _ false))]
       (responses/ok
-        {:time     (str (Date. (System/currentTimeMillis)))
-         :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
-         :app      {:status  "up"
+       {:time     (str (Date. (System/currentTimeMillis)))
+        :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
+        :app      {:status  "up"
                    :message ""}
-         :db       {:status  (if db-status "up" "down")
+        :db       {:status  (if db-status "up" "down")
                    :message (if db-status "" "Database connection failed")}}))
     (catch Exception e
       (responses/ok
-        {:time     (str (Date. (System/currentTimeMillis)))
-         :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
-         :app      {:status  "up"
+       {:time     (str (Date. (System/currentTimeMillis)))
+        :up-since (str (Date. (.getStartTime (java.lang.management.ManagementFactory/getRuntimeMXBean))))
+        :app      {:status  "up"
                    :message ""}
-         :db       {:status  "down"
+        :db       {:status  "down"
                    :message (.getMessage e)}}))))
