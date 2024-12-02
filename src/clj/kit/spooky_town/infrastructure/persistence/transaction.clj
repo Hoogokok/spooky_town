0528ac6bce7d7607a0573e360085f97843788526
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-02 15:45:58 +0900

    refactor(persistence): 트랜잭션 매니저에 읽기 전용 기능 추가

--- src/clj/kit/spooky_town/infrastructure/persistence/transaction.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/transaction.clj
@@ -6,12 +6,22 @@
   "Repository의 쿼리 함수를 트랜잭션 컨텍스트로 업데이트하기 위한 프로토콜"
   (update-query-fn [this query-fn]))
 
+(defprotocol TransactionManager
+  (with-tx [this f])
+  (with-read-only [this f]))
+
 (defrecord PgTransactionManager [conn]
+  TransactionManager
   (with-tx [this f]
     (jdbc/with-transaction [tx conn {:isolation :read-committed}]
       (let [query-fn (fn [sql params opts]
                        (jdbc/execute-one! tx (into [sql] params) opts))]
-        (f query-fn)))))
+        (f query-fn))))
+  
+  (with-read-only [this f]
+    (jdbc/with-transaction [tx conn {:isolation :read-committed
+                                    :read-only true}]
+      (f tx))))
 
 (defmethod ig/init-key :db/tx-manager [_ {:keys [conn]}]
   (->PgTransactionManager conn)) 
