ec5e54cc0ce9fcf32563e479b67ffcb25118ab7b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-03 06:53:41 +0900

    refactor(persistence): user 저장소 구현체 추가

---
+++ src/clj/kit/spooky_town/infrastructure/persistence/user.clj
@@ -0,0 +1,38 @@
+(ns kit.spooky-town.infrastructure.persistence.user
+  (:require [kit.spooky-town.domain.user.repository.protocol :as protocol]
+            [kit.spooky-town.infrastructure.persistence.transaction :refer [TransactionManager]]
+            [integrant.core :as ig]))
+
+(defrecord UserRepository [datasource tx-manager queries]
+  protocol/UserRepository
+  (save! [this user]
+    (.with-tx tx-manager
+      (fn [tx-query-fn]
+        (tx-query-fn {:uuid (:uuid user)
+                      :email (:email user)
+                      :password_hash (:hashed-password user)
+                      :name (:name user)}))))
+
+  (find-by-id [this id]
+    (.with-read-only tx-manager
+      (fn [tx-query-fn]
+        (tx-query-fn (:get-user-by-id queries) datasource {:id id}))))
+
+  (find-by-email [this email]
+    (.with-read-only tx-manager
+      (fn [tx-query-fn]
+        (tx-query-fn (:get-user-by-email queries) datasource {:email email}))))
+
+  (find-by-uuid [this uuid]
+    (.with-read-only tx-manager
+      (fn [tx-query-fn]
+        (tx-query-fn (:get-user-by-uuid queries) datasource {:uuid uuid}))))
+
+  (delete! [this id]
+    (.with-tx tx-manager
+      (fn [tx-query-fn]
+        (tx-query-fn (:delete-user! queries) datasource {:id id})))))
+
+(defmethod ig/init-key :infrastructure/user-repository
+  [_ {:keys [datasource tx-manager queries]}]
+  (->UserRepository datasource tx-manager queries)) 
