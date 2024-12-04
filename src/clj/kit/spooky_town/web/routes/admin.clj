624b6b1fec67d0a7a3d1eab2e229b467436e304f
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-04 10:29:34 +0900

    feat: 관리자 라우터 추가
    - /admin/users 엔드포인트 구현
      - 사용자 강제 탈퇴 (DELETE /:id) 추가
    - 관리자 권한 검증을 위한 bearer 토큰 설정
    - Malli 스키마로 요청/응답 정의

---
+++ src/clj/kit/spooky_town/web/routes/admin.clj
@@ -0,0 +1,21 @@
+(ns kit.spooky-town.web.routes.admin
+  (:require [kit.spooky-town.web.controllers.user :as user]))
+
+(defn admin-routes [{:keys [user-use-case]}]
+  ["/admin"
+   ["/users"
+    ["/:id"
+     {:delete {:handler (fn [req]
+                         (user/delete-user
+                          (assoc req :user-use-case user-use-case)))
+               :parameters {:path [:map [:id :string]]
+                          :body [:map [:reason :string]]}
+               :responses {204 {}
+                         400 {:body [:map [:error :string]]}
+                         403 {:body [:map [:error :string]]}
+                         404 {:body [:map [:error :string]]}
+                         500 {:body [:map [:error :string]]}}
+               :summary "관리자가 사용자를 탈퇴 처리합니다"
+               :description "관리자 권한으로 특정 사용자를 탈퇴 처리합니다."
+               :swagger {:tags ["admin"]
+                        :security [{:bearer []}]}}}]]]) 
