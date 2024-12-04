5547e5254ce94053b68345dd2f261c891fb37544
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-04 13:58:37 +0900

    feat(movie): 영화 장르 값 객체 정의 추가
    영화의 장르를 정의하는 값 객체를 추가합니다:
    - 주요 장르(primary-genre): horror, thriller 중 하나는 반드시 포함
    - 부가 장르(secondary-genre): psychological, supernatural 등 선택적 추가
    - 장르는 Set 형태로 저장되며 유효성 검증 함수 구현

--- src/clj/kit/spooky_town/domain/movie/value.clj
+++ src/clj/kit/spooky_town/domain/movie/value.clj
@@ -16,6 +16,21 @@
 (s/def ::directors (s/and (s/coll-of ::director :kind vector?)
                          #(pos? (count %))))  ;; 최소 1명 이상
 
+;; 장르 (필수로 호러나 스릴러를 포함해야 함)
+(s/def ::primary-genre #{:horror :thriller})
+(s/def ::secondary-genre #{:psychological   ;; 심리
+                          :supernatural    ;; 초자연
+                          :slasher        ;; 슬래셔
+                          :zombie         ;; 좀비
+                          :monster        ;; 몬스터
+                          :gore           ;; 고어
+                          :mystery})      ;; 미스터리
+
+(s/def ::genres (s/and (s/coll-of (s/or :primary ::primary-genre
+                                        :secondary ::secondary-genre)
+                                  :kind set?)
+                       #(some ::primary-genre %)))  ;; horror나 thriller 중 하나는 반드시 포함
+
 ;; 기본 생성 함수들
 (defn create-title [title]
   (when (s/valid? ::title title)
@@ -23,4 +38,8 @@
 
 (defn create-directors [directors]
   (when (s/valid? ::directors directors)
-    directors)) 
+    directors))
+
+(defn create-genres [genres]
+  (when (s/valid? ::genres genres)
+    genres)) 
