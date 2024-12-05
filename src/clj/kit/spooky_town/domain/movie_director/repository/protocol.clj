16554c0107c1bc3a1f5b0f5de9afd1689d01b595
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-05 17:05:33 +0900

    feat(repository): 영화-감독 관계 repository 프로토콜 추가

---
+++ src/clj/kit/spooky_town/domain/movie_director/repository/protocol.clj
@@ -0,0 +1,9 @@
+(ns kit.spooky-town.domain.movie-director.repository.protocol)
+
+(defprotocol MovieDirectorRepository
+  (save-movie-director! [this movie-id director-id role]
+    "영화-감독 관계 정보를 저장합니다.")
+  (find-directors-by-movie [this movie-id]
+    "영화 ID로 감독 정보를 조회합니다.")
+  (find-movies-by-director [this director-id]
+    "감독 ID로 영화 정보를 조회합니다.")) 
