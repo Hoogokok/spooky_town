fec1dc6ff9359947a3eb3c35942baa0abc94f888
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-05 22:00:17 +0900

    refactor(id): ULID 생성 함수 수정

--- test/clj/kit/spooky_town/domain/common/id/test/generator.clj
+++ test/clj/kit/spooky_town/domain/common/id/test/generator.clj
@@ -1,9 +1,9 @@
 (ns kit.spooky-town.domain.common.id.test.generator
   (:require [kit.spooky-town.domain.common.id.protocol :refer [IdGenerator]]))
 
-(defn generate-ulid [_ _]
+(defn generate-ulid [_]
   (throw (ex-info "Not implemented" {})))
 
 (defrecord TestIdGenerator []
   IdGenerator
-  (generate-ulid [this] (generate-ulid this nil))) 
+  (generate-ulid [this] (generate-ulid this))) 
