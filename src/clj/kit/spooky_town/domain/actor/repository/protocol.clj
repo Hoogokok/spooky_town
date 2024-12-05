d4c3cd07bdbe9f7f94d2f44c9332d0e29ca6e7b4
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-05 16:10:24 +0900

    feat(actor): 배우 이름으로 조회하는 기능 추가

--- src/clj/kit/spooky_town/domain/actor/repository/protocol.clj
+++ src/clj/kit/spooky_town/domain/actor/repository/protocol.clj
@@ -3,4 +3,5 @@
 (defprotocol ActorRepository
   (find-by-id [this id] "ID로 배우를 조회합니다.")
   (find-by-ids [this ids] "여러 ID로 배우들을 조회합니다.")
+  (find-by-name [this name] "이름으로 배우를 조회합니다.")
   (save! [this actor] "새로운 배우를 저장합니다.")) 
