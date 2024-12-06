e4cbc325886bf66e23ec6eae1df35920ce1760e9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-06 15:56:29 +0900

    refactor(movie): 영화 조회 테스트 레포지토리 메서드 추가 및 수정

--- test/clj/kit/spooky_town/domain/movie/test/repository.clj
+++ test/clj/kit/spooky_town/domain/movie/test/repository.clj
@@ -10,8 +10,16 @@
 (defn find-by-uuid [_ _]
   (throw (ex-info "Not implemented" {})))
 
+(defn find-by-criteria [_ _]
+  (throw (ex-info "Not implemented" {})))
+
+(defn count-by-criteria [_ _]
+  (throw (ex-info "Not implemented" {})))
+
 (defrecord TestMovieRepository []
   MovieRepository
   (save! [this movie] (save! this movie))
   (find-by-id [this id] (find-by-id this id))
-  (find-by-uuid [this uuid] (find-by-uuid this uuid))) 
+  (find-by-uuid [this uuid] (find-by-uuid this uuid))
+  (find-by-criteria [this criteria] (find-by-criteria this criteria))
+  (count-by-criteria [this criteria] (count-by-criteria this criteria))) 
