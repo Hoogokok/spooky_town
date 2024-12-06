77eba9a70c9914dd3ea062ff60df953d84e95465
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-06 22:14:59 +0900

    refactor(movie): 영화 라우트에 인증 처리 방식 수정

--- src/clj/kit/spooky_town/web/routes/movie.clj
+++ src/clj/kit/spooky_town/web/routes/movie.clj
@@ -1,38 +1,38 @@
 (ns kit.spooky-town.web.routes.movie
-  (:require [kit.spooky-town.web.controllers.movie :as movie]
-            [kit.spooky-town.web.middleware.auth :as auth]))
+  (:require [kit.spooky-town.web.controllers.movie :as movie] 
+            [kit.spooky-town.web.routes.common :refer [authenticated-route-data]]))
 
 (def movie-routes-data
   ["/movies"
    {:swagger {:tags ["movies"]}}
    
    [""
-    {:post {:summary "Create a new movie"
-            :parameters {:body [:map
-                              [:title string?]
-                              [:description {:optional true} string?]
-                              [:release_date string?]]}
-            :responses {201 {:body [:map [:movie-id string?]]}
-                       400 {:body [:map [:error string?]]}}
-            :middleware [auth/wrap-auth]
-            :handler movie/create-movie}
-     
-     :get {:summary "Search movies"
-           :parameters {:query [:map
-                              [:page {:optional true} pos-int?]
-                              [:sort-by {:optional true} keyword?]
-                              [:sort-order {:optional true} keyword?]
-                              [:title {:optional true} string?]
-                              [:director-name {:optional true} string?]
-                              [:actor-name {:optional true} string?]
-                              [:genres {:optional true} set?]
-                              [:release-status {:optional true} keyword?]]}
-           :responses {200 {:body [:map
-                                 [:movies vector?]
-                                 [:page pos-int?]
-                                 [:total-count nat-int?]
-                                 [:total-pages pos-int?]]}}
-           :handler movie/search-movies}}]
+    (merge authenticated-route-data
+           {:post {:summary "Create a new movie"
+                  :parameters {:body [:map
+                                    [:title string?]
+                                    [:description {:optional true} string?]
+                                    [:release_date string?]]}
+                  :responses {201 {:body [:map [:movie-id string?]]}
+                             400 {:body [:map [:error string?]]}}
+                  :handler movie/create-movie}
+            
+            :get {:summary "Search movies"
+                  :parameters {:query [:map
+                                     [:page {:optional true} pos-int?]
+                                     [:sort-by {:optional true} keyword?]
+                                     [:sort-order {:optional true} keyword?]
+                                     [:title {:optional true} string?]
+                                     [:director-name {:optional true} string?]
+                                     [:actor-name {:optional true} string?]
+                                     [:genres {:optional true} set?]
+                                     [:release-status {:optional true} keyword?]]}
+                  :responses {200 {:body [:map
+                                        [:movies vector?]
+                                        [:page pos-int?]
+                                        [:total-count nat-int?]
+                                        [:total-pages pos-int?]]}}
+                  :handler movie/search-movies}})]
    
    ["/:movie-id"
     {:get {:summary "Get movie details"
