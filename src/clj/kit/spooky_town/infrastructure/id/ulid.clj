0c18ced025a2d57623b4782b5fbef3c611bfe311
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-06 12:09:36 +0900

    refactor(id): ULID 생성 메서드 수정

--- src/clj/kit/spooky_town/infrastructure/id/ulid.clj
+++ src/clj/kit/spooky_town/infrastructure/id/ulid.clj
@@ -1,12 +1,14 @@
 (ns kit.spooky-town.infrastructure.id.ulid
-  (:require [com.github.f4b6a3/ulid-creator :as ulid]
-            [kit.spooky-town.domain.common.id.protocol :as protocol]
-            [integrant.core :as ig]))
+  (:require
+ [kit.spooky-town.domain.common.id.protocol :as protocol]
+   [integrant.core :as ig])
+ (:import
+  [com.github.f4b6a3.ulid UlidCreator]))
 
 (defrecord UlidGenerator []
   protocol/IdGenerator
   (generate-ulid [_]
-    (ulid/ulid)))
+    (.toString (UlidCreator/getUlid))))
 
-(defmethod ig/init-key ::ulid-generator [_ _]
+(defmethod ig/init-key :infrastructure/ulid-generator [_ _]
   (->UlidGenerator)) 
