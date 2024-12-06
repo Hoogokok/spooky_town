3248b17f4bea70142dac37424639b02c88a081bc
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-06 16:40:44 +0900

    fix(movie-director): 메서드 파라미터가 프로토콜과 맞지 않더 부분을 수정했다.

--- src/clj/kit/spooky_town/infrastructure/persistence/movie_director.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/movie_director.clj
@@ -1,14 +1,17 @@
 (ns kit.spooky-town.infrastructure.persistence.movie-director
-  (:require [kit.spooky-town.domain.movie-director.repository.protocol :as protocol]
-            [kit.spooky-town.infrastructure.persistence.transaction :refer [TransactionManager]]
+  (:require [kit.spooky-town.domain.movie-director.repository.protocol :as protocol] 
             [integrant.core :as ig]))
 
 (defrecord MovieDirectorRepository [datasource tx-manager queries]
   protocol/MovieDirectorRepository
-  (save-movie-director! [this director-movie]
+  (save-movie-director! [this movie-id director-id role]
     (.with-tx tx-manager
               (fn [tx-query-fn]
-                (tx-query-fn (:save-director-movie! queries) datasource director-movie))))
+                (tx-query-fn (:save-director-movie! queries) 
+                            datasource 
+                            {:movie_id movie-id
+                             :director_id director-id
+                             :role role}))))
 
   (find-directors-by-movie [this movie-id]
     (.with-read-only tx-manager
