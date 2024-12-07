e53d79d7e9f1824c8650da759dd4d792673b9c63
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-07 10:17:00 +0900

    refactor(role): Role 엔티티 스키마 수정
    Role 엔티티 스키마를 수정하였습니다. 변경된 스키마에는 role-id, role-name 외에도 description, created-at, updated-at 필드가 추가되었습니다.
    또한, Role 엔티티에 권한 체크와 업데이트를 위한 메서드들이 추가되었습니다.

--- src/clj/kit/spooky_town/domain/role/entity.clj
+++ src/clj/kit/spooky_town/domain/role/entity.clj
@@ -1,19 +1,42 @@
 (ns kit.spooky-town.domain.role.entity
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [kit.spooky-town.domain.role.value :as value]))
 
 ;; Entity Spec
-(s/def ::role-id int?)
-(s/def ::role-name string?)
-
 (s/def ::role
-  (s/keys :req-un [::role-id
-                   ::role-name]))
+  (s/keys :req-un [::value/role-id
+                   ::value/role-name]
+          :opt-un [::value/description
+                   ::value/created-at
+                   ::value/updated-at]))
 
-(defrecord Role [role-id role-name])
+(defrecord Role [role-id role-name description created-at updated-at])
 
 (defn create-role
-  [{:keys [role-id role-name]}]
+  [{:keys [role-id role-name description created-at]
+    :or {created-at (value/create-timestamp)}}]
   (let [role (map->Role {:role-id role-id
-                        :role-name role-name})]
+                         :role-name (value/create-role-name role-name)
+                         :description description
+                         :created-at created-at
+                         :updated-at created-at})]
     (when (s/valid? ::role role)
-      role))) 
+      role)))
+
+;; Role 권한 체크 메서드들
+(defn can-manage-content? [^Role role]
+  (value/can-manage-content? (:role-name role)))
+
+(defn can-publish-sns? [^Role role]
+  (value/can-publish-sns? (:role-name role)))
+
+(defn can-manage-system? [^Role role]
+  (value/can-manage-system? (:role-name role)))
+
+;; Role 업데이트 메서드들
+(defn update-description
+  [^Role role new-description]
+  (when-let [validated-description (value/create-description new-description)]
+    (map->Role (assoc role 
+                     :description validated-description
+                     :updated-at (value/create-timestamp))))) 
