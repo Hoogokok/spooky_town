bb7023cd1cdd3b3bbba8ae14d6ab846839cd98b3
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-08 22:10:36 +0900

    refactor(persistence): 배우 저장소 트랜잭션 처리 방식 개선
    - 직접적인 JDBC 호출을 제거하고 트랜잭션 매니저를 통한 일관된 접근 방식으로 변경
    - 읽기 작업은 .with-read-only, 쓰기 작업은 .with-tx를 사용하여 명시적 구분
    - UpdateQueryFn 프로토콜 구현 추가
    - result-set 변환 옵션 추가
    
    이 변경으로 배우 저장소의 모든 데이터베이스 작업이 트랜잭션 매니저를 통해
    안전하고 일관되게 처리되도록 개선되었습니다.

--- src/clj/kit/spooky_town/infrastructure/persistence/actor.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/actor.clj
@@ -1,24 +1,38 @@
 (ns kit.spooky-town.infrastructure.persistence.actor
   (:require [kit.spooky-town.domain.actor.repository.protocol :as protocol]
-            [kit.spooky-town.infrastructure.persistence.transaction :refer [TransactionManager]]
-            [integrant.core :as ig]))
+            [kit.spooky-town.infrastructure.persistence.transaction :refer [UpdateQueryFn]]
+            [integrant.core :as ig]
+            [next.jdbc.result-set :as rs]))
 
 (defrecord ActorRepository [datasource tx-manager queries]
+  UpdateQueryFn
+  (update-query-fn [this tx-fn]
+    (assoc this :query-fn tx-fn))
+
   protocol/ActorRepository
   (save! [this actor]
-    (.with-tx tx-manager
-              (fn [tx-query-fn]
-                (tx-query-fn (:save-actor! queries) datasource actor))))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:save-actor! queries)
+                   actor
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-id [this id]
-    (.with-read-only tx-manager
-                     (fn [tx-query-fn]
-                       (tx-query-fn (:get-actor-by-id queries) datasource {:actor_id id}))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-actor-by-id queries)
+                   {:id id}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-name [this name]
-    (.with-read-only tx-manager
-                     (fn [tx-query-fn]
-                       (tx-query-fn (:get-actor-by-name queries) datasource {:name name})))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-actor-by-name queries)
+                   {:name name}
+                   {:builder-fn rs/as-unqualified-maps}))))))
 
 (defmethod ig/init-key :infrastructure/actor-repository
   [_ {:keys [datasource tx-manager queries]}]
