9551a4fde70360a88dca83687681bc8bd716baee
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-08 22:05:48 +0900

    refactor(persistence): 영화 저장소 트랜잭션 처리 방식 개선
    - 직접적인 JDBC 호출을 제거하고 트랜잭션 매니저를 통한 일관된 접근 방식으로 변경
    - 읽기 작업은 .with-read-only, 쓰기 작업은 .with-tx를 사용하여 명시적 구분
    - UpdateQueryFn 프로토콜 구현 추가
    
    이 변경으로 영화 저장소의 모든 데이터베이스 작업이 트랜잭션 매니저를 통해
    안전하고 일관되게 처리되도록 개선되었습니다.

--- src/clj/kit/spooky_town/infrastructure/persistence/movie.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/movie.clj
@@ -1,24 +1,38 @@
 (ns kit.spooky-town.infrastructure.persistence.movie
   (:require [kit.spooky-town.domain.movie.repository.protocol :as protocol]
-            [kit.spooky-town.infrastructure.persistence.transaction :refer [TransactionManager]]
-            [integrant.core :as ig]))
+            [kit.spooky-town.infrastructure.persistence.transaction :refer [UpdateQueryFn]]
+            [integrant.core :as ig]
+            [next.jdbc.result-set :as rs]))
 
 (defrecord MovieRepository [datasource tx-manager queries]
+  UpdateQueryFn
+  (update-query-fn [this tx-fn]
+    (assoc this :query-fn tx-fn))
+
   protocol/MovieRepository
   (save! [this movie]
-    (.with-tx tx-manager
-      (fn [tx-query-fn]
-        (tx-query-fn (:save-movie! queries) datasource movie))))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:save-movie! queries)
+                   movie
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-id [this id]
-    (.with-read-only tx-manager
-      (fn [tx-query-fn]
-        (tx-query-fn (:get-movie-by-id queries) datasource {:id id}))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-movie-by-id queries)
+                   {:id id}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-uuid [this uuid]
-    (.with-read-only tx-manager
-      (fn [tx-query-fn]
-        (tx-query-fn (:get-movie-by-uuid queries) datasource {:uuid uuid})))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-movie-by-uuid queries)
+                   {:uuid uuid}
+                   {:builder-fn rs/as-unqualified-maps}))))))
 
 (defmethod ig/init-key :infrastructure/movie-repository
   [_ {:keys [datasource tx-manager queries]}]
