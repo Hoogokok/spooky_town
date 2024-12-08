a36de3538941e0fec9104bcf725550b53c8d24b9
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-08 22:13:54 +0900

    refactor(persistence): 영화-감독 저장소 트랜잭션 처리 방식 개선
    - 직접적인 JDBC 호출을 제거하고 트랜잭션 매니저를 통한 일관된 접근 방식으로 변경
    - 읽기 작업은 .with-read-only, 쓰기 작업은 .with-tx를 사용하여 명시적 구분
    - UpdateQueryFn 프로토콜 구현 추가
    - result-set 변환 옵션 추가
    
    이 변경으로 영화-감독 저장소의 모든 데이터베이스 작업이 트랜잭션 매니저를 통해
    안전하고 일관되게 처리되도록 개선되었습니다.

--- src/clj/kit/spooky_town/infrastructure/persistence/movie_director.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/movie_director.clj
@@ -1,27 +1,40 @@
 (ns kit.spooky-town.infrastructure.persistence.movie-director
-  (:require [kit.spooky-town.domain.movie-director.repository.protocol :as protocol] 
-            [integrant.core :as ig]))
+  (:require [kit.spooky-town.domain.movie-director.repository.protocol :as protocol]
+            [kit.spooky-town.infrastructure.persistence.transaction :refer [UpdateQueryFn]]
+            [integrant.core :as ig]
+            [next.jdbc.result-set :as rs]))
 
 (defrecord MovieDirectorRepository [datasource tx-manager queries]
+  UpdateQueryFn
+  (update-query-fn [this tx-fn]
+    (assoc this :query-fn tx-fn))
+
   protocol/MovieDirectorRepository
   (save-movie-director! [this movie-id director-id role]
-    (.with-tx tx-manager
-              (fn [tx-query-fn]
-                (tx-query-fn (:save-director-movie! queries) 
-                            datasource 
-                            {:movie_id movie-id
-                             :director_id director-id
-                             :role role}))))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:save-director-movie! queries)
+                   {:movie_id movie-id
+                    :director_id director-id
+                    :role role}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-directors-by-movie [this movie-id]
-    (.with-read-only tx-manager
-                     (fn [tx-query-fn]
-                       (tx-query-fn (:get-directors-by-movie-id queries) datasource {:movie_id movie-id}))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-directors-by-movie-id queries)
+                   {:movie_id movie-id}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-movies-by-director [this director-id]
-    (.with-read-only tx-manager
-                     (fn [tx-query-fn]
-                       (tx-query-fn (:get-movies-by-director-id queries) datasource {:director_id director-id})))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-movies-by-director-id queries)
+                   {:director_id director-id}
+                   {:builder-fn rs/as-unqualified-maps}))))))
 
 (defmethod ig/init-key :infrastructure/movie-director-repository
   [_ {:keys [datasource tx-manager queries]}]
