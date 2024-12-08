c93c561b37490af171d9566004bb6001ec095185
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-08 21:57:42 +0900

    refactor(persistence): 사용자 저장소 트랜잭션 처리 방식 개선
    - 직접적인 JDBC 호출을 제거하고 트랜잭션 매니저를 통한 일관된 접근 방식으로 변경
    - 읽기 작업은 .with-read-only, 쓰기 작업은 .with-tx를 사용하여 명시적 구분
    - 모든 메서드에서 일관된 트랜잭션 처리 패턴 적용
    
    이 변경으로 사용자 저장소의 모든 데이터베이스 작업이 트랜잭션 매니저를 통해
    안전하고 일관되게 처리되도록 개선되었습니다.

--- src/clj/kit/spooky_town/infrastructure/persistence/user.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/user.clj
@@ -2,7 +2,6 @@
   (:require [kit.spooky-town.domain.user.repository.protocol :as protocol]
             [kit.spooky-town.infrastructure.persistence.transaction :refer [UpdateQueryFn]]
             [integrant.core :as ig]
-            [next.jdbc :as jdbc]
             [next.jdbc.result-set :as rs]))
 
 (defrecord UserRepository [datasource tx-manager queries]
@@ -12,63 +11,63 @@
 
   protocol/UserRepository
   (save! [this user]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:save-user! queries)
-                {:uuid (:uuid user)
-                 :email (:email user)
-                 :password_hash (:hashed-password user)
-                 :name (:name user)}
-                {:builder-fn rs/as-unqualified-maps})))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:save-user! queries)
+                   {:uuid (:uuid user)
+                    :email (:email user)
+                    :password_hash (:hashed-password user)
+                    :name (:name user)}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-id [this id]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:get-user-by-id queries)
-                {:id id}
-                {:builder-fn rs/as-unqualified-maps})))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-user-by-id queries)
+                   {:id id}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-email [this email]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:get-user-by-email queries)
-                {:email email}
-                {:builder-fn rs/as-unqualified-maps})))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-user-by-email queries)
+                   {:email email}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-by-uuid [this uuid]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:get-user-by-uuid queries)
-                {:uuid uuid}
-                {:builder-fn rs/as-unqualified-maps})))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:get-user-by-uuid queries)
+                   {:uuid uuid}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (delete! [this id]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:delete-user! queries)
-                {:id id}
-                {:builder-fn rs/as-unqualified-maps})))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:delete-user! queries)
+                   {:id id}
+                   {:builder-fn rs/as-unqualified-maps})))))
 
   (find-id-by-uuid [this uuid]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (:id (query-fn (:find-user-id-by-uuid queries)
-                     {:uuid uuid}
-                     {:builder-fn rs/as-unqualified-maps}))))
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (:id (query-fn (:find-user-id-by-uuid queries)
+                        {:uuid uuid}
+                        {:builder-fn rs/as-unqualified-maps}))))))
   
   (mark-as-withdrawn [this user]
-    (let [query-fn (or (:query-fn this)
-                      (fn [sql params opts]
-                        (jdbc/execute-one! datasource (into [sql] params) opts)))]
-      (query-fn (:mark-user-as-withdrawn queries)
-                user
-                {:builder-fn rs/as-unqualified-maps}))))
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:mark-user-as-withdrawn queries)
+                   user
+                   {:builder-fn rs/as-unqualified-maps}))))))
 
 (defmethod ig/init-key :infrastructure/user-repository
   [_ {:keys [datasource tx-manager queries]}]
