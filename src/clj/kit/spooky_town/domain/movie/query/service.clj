5ae86bdd40ed50ccc6fde97e0a8d88dad8bbd24c
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-09 13:32:26 +0900

    refactor: 트랜잭션 관리 변경을 위해 with-read-only 함수를 사용하여 코드 수정

--- src/clj/kit/spooky_town/domain/movie/query/service.clj
+++ src/clj/kit/spooky_town/domain/movie/query/service.clj
@@ -28,10 +28,13 @@
     "영화 요약 정보를 조회합니다.
      params: {:movie-id string?}"))
 
-(defrecord MovieQueryServiceImpl [movie-repository movie-actor-repository movie-director-repository tx-manager]
+(defrecord MovieQueryServiceImpl [movie-repository 
+                                movie-actor-repository 
+                                movie-director-repository 
+                                with-read-only]
   MovieQueryService
   (find-movie [_ params]
-    (.with-read-only-transaction tx-manager [movie-repository movie-actor-repository movie-director-repository]
+    (with-read-only [movie-repository movie-actor-repository movie-director-repository]
       (fn [movie-repo actor-repo director-repo]
         (when-let [movie (movie-repo/find-by-id movie-repo (:movie-id params))]
           (cond-> movie
@@ -42,7 +45,7 @@
             (assoc :directors (movie-director-repo/find-directors-by-movie director-repo (:movie-id params))))))))
 
   (search-movies [_ params]
-    (.with-read-only-transaction tx-manager [movie-repository]
+    (with-read-only [movie-repository]
       (fn [repo]
         (let [movies (movie-repo/find-by-criteria repo params)
               total-count (movie-repo/count-by-criteria repo params)]
@@ -52,11 +55,15 @@
            :total-pages (int (Math/ceil (/ total-count 12.0)))}))))
 
   (get-movie-summary [_ params]
-    (.with-read-only-transaction tx-manager [movie-repository]
+    (with-read-only [movie-repository]
       (fn [repo]
         (when-let [movie (movie-repo/find-by-id repo (:movie-id params))]
           (entity/->summary movie))))))
 
 (defmethod ig/init-key :domain/movie-query-service
-  [_ {:keys [movie-repository movie-actor-repository movie-director-repository tx-manager]}]
-  (->MovieQueryServiceImpl movie-repository movie-actor-repository movie-director-repository tx-manager))
+  [_ {:keys [movie-repository movie-actor-repository movie-director-repository with-read-only]}]
+  (->MovieQueryServiceImpl 
+    movie-repository 
+    movie-actor-repository 
+    movie-director-repository 
+    with-read-only))
