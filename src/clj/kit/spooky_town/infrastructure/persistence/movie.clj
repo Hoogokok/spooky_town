4a3e0de24297496c1a6357576082c1ba5cee621b
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-12-11 11:00:44 +0900

    feat(movie): 영화 레포지토리 구현체에 삭제 및 조회 기능 추가
    - MovieRepository 구현체에 프로토콜 메서드 구현
      - mark-as-deleted! 메서드 구현
      - find-by-criteria 메서드 구현
      - count-by-criteria 메서드 구현
    
    - SQL 쿼리 추가 및 수정
      - mark-movie-as-deleted! 쿼리 수정 (is_deleted 필드 추가)
      - find-movies-by-criteria 쿼리 추가 (페이징, 정렬, 필터링 지원)
      - count-movies-by-criteria 쿼리 추가

--- src/clj/kit/spooky_town/infrastructure/persistence/movie.clj
+++ src/clj/kit/spooky_town/infrastructure/persistence/movie.clj
@@ -32,7 +32,32 @@
         (let [query-fn (:query-fn repo)]
           (query-fn (:get-movie-by-uuid queries)
                    {:uuid uuid}
-                   {:builder-fn rs/as-unqualified-maps}))))))
+                   {:builder-fn rs/as-unqualified-maps})))))
+
+  (mark-as-deleted! [this movie-id timestamp]
+    (.with-tx tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:mark-movie-as-deleted! queries)
+                   {:movie_id movie-id
+                    :deleted_at timestamp}
+                   {:builder-fn rs/as-unqualified-maps})))))
+
+  (find-by-criteria [this {:keys [include-deleted?] :as criteria}]
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (query-fn (:find-movies-by-criteria queries)
+                   (assoc criteria :include_deleted include-deleted?)
+                   {:builder-fn rs/as-unqualified-maps})))))
+
+  (count-by-criteria [this criteria]
+    (.with-read-only tx-manager [this]
+      (fn [repo]
+        (let [query-fn (:query-fn repo)]
+          (:count (query-fn (:count-movies-by-criteria queries)
+                           criteria
+                           {:builder-fn rs/as-unqualified-maps})))))))
 
 (defmethod ig/init-key :infrastructure/movie-repository
   [_ {:keys [datasource tx-manager queries]}]
